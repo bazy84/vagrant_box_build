a8517014b6b9a8994c44b374f8a0ff40f601ac6b
Nicolae Critu <nicolae.critu@unifiedpost.com>
2014-05-27 17:15:47 +0300

    changed ISO location and EPEL url

--- templates/centos-6.5-x86_64-standard/scripts/epel.sh
+++ templates/centos-6.5-x86_64-standard/scripts/epel.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-yum install -y http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+yum install -y http://ftp.astral.ro/mirrors/fedora/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+
